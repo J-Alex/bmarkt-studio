6db0f46f6ab1cae2794b76c3530015d9443ab886
Brandon Josué Blandón Vargas <Brandon Josu Blandn Vargas>
2017-02-13 15:13:48 -0600

    asda

--- public/api/mail-api.php
+++ public/api/mail-api.php
@@ -1,46 +1,11 @@
 <?php
-// Varios destinatarios
-$para  = 'brjosue73@gmail.com' . ', '; // atención a la coma
-$para .= 'wez@example.com';
 
-// título
-$título = 'Recordatorio de cumpleaños para Agosto';
+$mensaje = "Línea 1\r\nLínea 2\r\nLínea 3";
 
-// mensaje
-$mensaje = '
-<html>
-<head>
-  <title>Recordatorio de cumpleaños para Agosto</title>
-</head>
-<body>
-  <p>¡Estos son los cumpleaños para Agosto!</p>
-  <table>
-    <tr>
-      <th>Quien</th><th>Día</th><th>Mes</th><th>Año</th>
-    </tr>
-    <tr>
-      <td>Joe</td><td>3</td><td>Agosto</td><td>1970</td>
-    </tr>
-    <tr>
-      <td>Sally</td><td>17</td><td>Agosto</td><td>1973</td>
-    </tr>
-  </table>
-</body>
-</html>
-';
+// Si cualquier línea es más larga de 70 caracteres, se debería usar wordwrap()
+$mensaje = wordwrap($mensaje, 70, "\r\n");
 
-// Para enviar un correo HTML, debe establecerse la cabecera Content-type
-$cabeceras  = 'MIME-Version: 1.0' . "\r\n";
-$cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-// Cabeceras adicionales
-$cabeceras .= 'To: Mary <mary@example.com>, Kelly <kelly@example.com>' . "\r\n";
-$cabeceras .= 'From: Recordatorio <cumples@example.com>' . "\r\n";
-$cabeceras .= 'Cc: birthdayarchive@example.com' . "\r\n";
-$cabeceras .= 'Bcc: birthdaycheck@example.com' . "\r\n";
-
-// Enviarlo
-if(mail($para, $título, $mensaje, $cabeceras)){
+if(mail('caffeinated@example.com', 'Mi título', $mensaje)){
 	echo "si";
 }
 else {
