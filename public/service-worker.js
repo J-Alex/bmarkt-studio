c0a63748202dc4f9ef8fa33cf9a972e1250c28b1
J-Alex <jlex.wd@outlook.com>
2017-03-21 17:30:03 -0600

    service worker - offline apps

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,8 @@
+'use strict';
 
-/*var cache_name = "cacheando";
-var cache_urls = ["index.html","res/css/styles.min.css","res/js/angular.min.js","res/js/jquery.min.js","res/js/scripts.min.js","res/js/angular-ui-router.js", "res/img/banner.png", "res/img/Banner1.png", "res/img/banner.png"];
+
+var cache_name = "cacheando";
+var cache_urls = ["res/fonts/Arial-Rounded-Bold.ttf", "res/fonts/Arial-Rounded.ttf", "res/fonts/Montserrat-Regular.ttf", "res/fonts/fontawesome-webfont.ttf", "index.html", "service-worker.js", "res/css/styles.min.css","res/css/font-awesome.min.css","res/css/jquery.bxslider.min.css","res/js/angular.min.js","res/js/jquery.min.js","res/js/scripts.min.js","res/js/angular-ui-router.js","res/js/lazyload.js","res/js/isotope.pkgd.min.js","res/js/jquery.bxslider.min.js","res/js/isotope.js", "res/img/banner.png", "res/img/Banner1.png", "res/img/banner.png", "res/img/services-home/diseno2.png", "res/img/services-home/desarrollo.png", "res/img/wide/architect.jpg", "res/img/wide/design.jpg", "res/img/wide/development.jpg", "res/img/wide/marketing.jpg"];
 
 self.addEventListener('install', function(ev){
 	console.log("cache instalada");
@@ -9,6 +11,21 @@ self.addEventListener('install', function(ev){
 			console.log("cacheando");
 			return cache.addAll(cache_urls)
 		});
+});
+
+self.addEventListener('activate', function(e) {
+	console.log('activando');
+	/*e.waitUntil(
+		caches.keys().then(function(cache_names){
+			return Promise.all(
+				cache_name.map(function(cache_nama) {
+					if(cache_name == cache_nama) {
+						return caches.delete(cache_nama)
+					}
+				})
+			)
+		})
+	)*/
 });
 
 self.addEventListener("fetch", function(e) {
@@ -23,6 +40,10 @@ self.addEventListener("fetch", function(e) {
 
 				//console.log("NO estoy en el caché");
 				return fetch(e.request);
+			}).catch(function(err){
+				if(ev.request.mode == "navigate") {
+					return caches.match("index.html");
+				}
 			})
 	)
-});*/
+});
--- src/js/load.js
+++ src/js/load.js
@@ -1,10 +1,14 @@
 ;(function(){
-  /*if ('serviceWorker' in navigator) {
+  //if ('serviceWorker' in navigator) {
+  if(navigator.serviceWorker) {
+  	console.log("si hay serviceWorker")
     navigator.serviceWorker.register('service-worker.js').then(function(registration) {
       console.log('Service Worker registered');
     }).catch(function(err) {
       console.log('Service Worker registration failed: ', err);
     });
-  }*/
+  } else {
+  	console.log("no hay serviceWorker");
+  }
 
 })();
