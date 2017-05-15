511190abb88643a35ffd0f69a5cacef36695d298
J-Alex <jlex.wd@outlook.com>
2017-05-15 16:27:58 -0600

    allahu akbar 15 de mayo

--- public/res/js/app.js
+++
@@ -1,8 +0,0 @@
-
-require('./jquery-3.2.0.min.js');
-require('./angular-1.6.1.min.js');
-require('./angular-ui-router.js');
-require('./isotope.pkgd.min.js');
-require('./jquery.bxslider.min.js');
-require('./scripts.min.js');
-require('./lazyload.js');
