ca490f3f0894e8a7c678e8ee561ed9aba6c2008a
J-Alex <jlex.wd@outlook.com>
2017-05-18 17:12:50 -0600

    meta description service seo angular

--- src/js/controllers/headCtrl.js
+++ src/js/controllers/headCtrl.js
@@ -9,10 +9,13 @@
 	
 	function headCtrl(seoTitleService, $scope){
 
-	    var titulo = "BmarktStudio - Diseño Gráfico, Desarrollo Web, Marketing, Arquitectura"
+	    var titulo = "BmarktStudio - Diseño Gráfico, Desarrollo Web, Marketing, Arquitectura";
+	    var homeDescription = "En Bmarkt, nuestra misión es transformarte digitalmente, ponemos en las manos de nuestros clientes las oportunidades de la era moderna";
+	    
 	    seoTitleService.setTitle(titulo);
-	    console.log(seoTitleService.title());
+	    seoTitleService.setMetaDescription(homeDescription);
 
 		$scope.seoTitle = seoTitleService.title();
+		$scope.homeDescription = seoTitleService.metaDescription();
 	}
 })();
